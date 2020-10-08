9d898829006efa3c9484fcd043c86ec1658ca61c
ycfcando <1319646692@qq.com>
2020-10-08 11:33:00 +0800

    v1.0.7

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,5 +2,5 @@ import http from './http';
 
 //mine接口
 export function getMine(url, params) {
-  return http.get('https://www.fastmock.site/mock/7ae3547b41ba9b838aa668af3ad08107/mine/mine', params);
+  return http.get('/api/lines/userrecommends', params);
 }
